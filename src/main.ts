a51216e9f43b5bb5f15b3f3845f54c073946cf49
atilamariano <atilamariano@hotmail.com>
2023-04-18 19:03:04 -0300

    feat: installing and configuring swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('CAF Nest API')
+    .setDescription('API criada em Nest.JS referente ao bootcamp II da BLUE')
+    .setVersion('1.0')
+    .addServer('http://localhost:3333/docs')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
