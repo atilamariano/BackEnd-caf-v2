310ecacd9640c3ee8c23a84afa21b22bf6c6a14f
atilamariano <atilamariano@hotmail.com>
2023-04-20 21:35:54 -0300

    fix: error resolved

---
+++ dist/user/user.service.d.ts
@@ -0,0 +1,12 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+export declare class UserService {
+    private readonly prisma;
+    constructor(prisma: PrismaService);
+    create(createUserDto: CreateUserDto): string;
+    findAll(): string;
+    findOne(id: number): string;
+    update(id: number, updateUserDto: UpdateUserDto): string;
+    remove(id: number): string;
+}
